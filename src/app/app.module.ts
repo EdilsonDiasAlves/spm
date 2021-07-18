7443b69d7b4c9d5700f98f3a423a791f5b5e9150
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-07-18 01:33:56 -0300

    Improving the product-list component: 1. Defining Interfaces 2. Encapsulating Component Styles 3. Using Lifecycle Hooks 3. Building Custom Pipes 4. Getters and Setters 5. Filtering and Arrow Functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,7 +9,8 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces
   ],
   imports: [
     BrowserModule,
