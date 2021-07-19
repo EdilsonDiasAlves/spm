1c46107375fe01f62cece46b879b54e2594526ec
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-07-19 20:04:45 -0300

    Adding basic navigation:
    1. Configuring Routes
    2. Tying Routes to Actions
    3. Placing the Views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { WelcomeComponent } from './home/welcome.component';
 import { StarComponent } from './shared/star.component';
 import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 import { ProductListComponent } from './products/product-list.component';
@@ -7,18 +8,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpaces,
-    StarComponent
+    StarComponent,
+    WelcomeComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
   ],
   bootstrap: [AppComponent]
 })
