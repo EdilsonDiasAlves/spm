b6d540ec5781d0f2945e67fdca1e097ac2ca3438
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-07-20 17:59:58 -0300

    substancial changes - design and navigation to details

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router) { }
+
+  product: IProduct | undefined;
   pageTitle: string = "Product Detail"
+
   ngOnInit(): void {
+    const productId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${productId}`;
+    this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2021",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
